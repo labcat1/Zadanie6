07708e7f05aa5c8812c1c6ca1847618e3912c48a
labcat_ <boldyrev0205@gmail.com>
2026-03-05 13:13:59 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#define max 100
+#define MAXIM 100
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
 bool theInput(int* array, int size) {
    int temp = 0;
    while (temp < size) {
@@ -16,9 +19,7 @@ int Max(int* array, int size) {
    int temp = 0;
    int current_maximum=array[0];
    while (temp < size) {
-      if (array[temp] > current_maximum) {
-         current_maximum = array[temp];
-      }
+      current_maximum = MAX(current_maximum, array[temp]);
       temp++;
    }
    return current_maximum;
@@ -28,9 +29,7 @@ int Min(int* array, int size) {
    int temp = 0;
    int current_minimum= array[0];
    while (temp < size) {
-      if (array[temp] < current_minimum) {
-         current_minimum = array[temp];
-      }
+      current_minimum = MIN(current_minimum, array[temp]);
       temp++;
    }
    return current_minimum;
@@ -61,19 +60,24 @@ int main(void) {
    int size;
    printf("Input number of elements:");
    scanf("%d", &size);
-   if (size > max) {
+   if (size > MAXIM) {
       printf("Your array is too big!");
    }
    else {
-      int* array = (int*)malloc(size*sizeof(int));
-      if (array == 0) {
-         printf("ERROR: COULDN'T ALLOCATE MEMORY");
+      int array[MAXIM];
+
+      if (size == 0) {
+         printf("\nMaxinum:0");
+         printf("\nMinimum:0");
+         printf("\nMean:0");
+         printf("\nRMS:0");
       }
-      theInput(array, size);
-      printf("Maxinum:%d", Max(array, size));
-      printf("\nMinimum:%d", Min(array, size));
-      printf("\nMean:%f", Mean(array, size));
-      printf("\nRMS:%f", RMS(array, size));
-      free(array);
+      else{
+         theInput(array, size);
+         printf("Maxinum:%d", Max(array, size));
+         printf("\nMinimum:%d", Min(array, size));
+         printf("\nMean:%f", Mean(array, size));
+         printf("\nRMS:%f", RMS(array, size));
+      }
    }
 }
